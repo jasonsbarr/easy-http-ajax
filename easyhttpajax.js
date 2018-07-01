651c6da3d21122ec22e1f4ede1725f8268210f40
Jason Barr <jason@jasonsbarr.com>
2018-06-30 23:50:53 -0500

    Add basic functionality for get(), document function params & callback

--- easyhttpajax.js
+++ easyhttpajax.js
@@ -1,7 +1,8 @@
 /**
- * @file    A simple wrapper API for XMLHttpRequest to make 
- *          HTTP requests of different types
+ * A simple wrapper API for XMLHttpRequest to make asynchronous 
+ * HTTP requests of different types
  * 
+ * @file API functions library
  * @author Jason Barr
  * @version 0.1
  * @license MIT
@@ -10,7 +11,7 @@
 /**
  * Instantiates EasyHTTP library
  * @constructor
- * @property {XMLHttpRequest} http    Request object this class wraps around
+ * @property {XMLHttpRequest} http Request object this class wraps around
  */
 let EasyHTTP = function() {
     this.http = new XMLHttpRequest();
@@ -18,10 +19,32 @@ let EasyHTTP = function() {
 
 /**
  * Makes HTTP GET request
+ * 
+ * @param {string} url
+ * @param {responseCallback} callback Callback function to handle data response
  */
-EasyHTTP.prototype.get = function() {
+EasyHTTP.prototype.get = function(url, callback) {
+    let self = this;
 
+    this.http.open('GET', url, true);
+
+    this.http.onload = function() {
+        if (self.http.status === 200) {
+            callback(self.http.responseText);
+        } else {
+            callback(null, `Error: ${self.http.status}`)
+        }
+    }
+
+    this.http.send();
 }
+/**
+ * Response callback requirements for EasyHTTP.get()
+ * 
+ * @callback responseCallback
+ * @param {string} responseText HTTP Request response text
+ * @param {string} [errorMessage] Error message HTTP status
+ */
 
 /**
  * Makes HTTP POST request
@@ -41,5 +64,5 @@ EasyHTTP.prototype.put = function() {
  * Makes HTTP DELETE request
  */
 EasyHTTP.prototype.delete = function() {
-    
+
 }
