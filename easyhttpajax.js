38fbcfb2ed3900c08b6c2b33f6a5cac7e6bb4ee7
Jason Barr <jason@jasonsbarr.com>
2018-07-01 00:13:18 -0500

    Add basic functionality for put()

--- easyhttpajax.js
+++ easyhttpajax.js
@@ -21,7 +21,7 @@ let EasyHTTP = function() {
  * Makes HTTP GET request
  * 
  * @param {string} url
- * @param {responseCallback} callback Callback function to handle data response
+ * @param {getResponseCallback} callback Callback function to handle data response
  */
 EasyHTTP.prototype.get = function(url, callback) {
     let self = this;
@@ -41,7 +41,7 @@ EasyHTTP.prototype.get = function(url, callback) {
 /**
  * Response callback requirements for EasyHTTP.get()
  * 
- * @callback responseCallback
+ * @callback getResponseCallback
  * @param {string} responseText HTTP Request response text
  * @param {string} [errorMessage] Error message HTTP status
  */
@@ -51,7 +51,7 @@ EasyHTTP.prototype.get = function(url, callback) {
  * 
  * @param {string} url
  * @param {Object} data
- * @param {requestCallback} callback
+ * @param {postRequestCallback} callback
  */
 EasyHTTP.prototype.post = function(url, data, callback) {
     let self = this;
@@ -62,15 +62,37 @@ EasyHTTP.prototype.post = function(url, data, callback) {
         callback(self.http.responseText);
     }
 
-    this.send(JSON.stringify(data));
+    this.http.send(JSON.stringify(data));
 }
+/**
+ * POST request callback requirements for EasyHTTP.post()
+ * @callback postRequestCallback
+ * @param {string} responseText HTTP response text
+ */
 
 /**
- * Makes HTTP PUT request
+ * Makes HTTP POST request
+ * 
+ * @param {string} url
+ * @param {Object} data
+ * @param {postRequestCallback} callback
  */
-EasyHTTP.prototype.put = function() {
+EasyHTTP.prototype.put = function(url, data, callback) {
+    let self = this;
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-Type', 'application/json');
 
+    this.http.onload = function() {
+        callback(self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
 }
+/**
+ * PUT request callback requirements for EasyHTTP.put()
+ * @callback putRequestCallback
+ * @param {string} responseText HTTP response text
+ */
 
 /**
  * Makes HTTP DELETE request
