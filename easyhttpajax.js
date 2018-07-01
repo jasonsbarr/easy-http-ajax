f68443422de230f680551906534a63e5d60876cd
Jason Barr <jason@jasonsbarr.com>
2018-07-01 02:40:46 -0500

    Refactor to object namespace with private XMLHttpRequest object

--- easyhttpajax.js
+++ easyhttpajax.js
@@ -3,115 +3,123 @@
  * HTTP requests of different types to handle JSON data
  * 
  * @file API functions library
+ * @namespace EasyHTTPAjax
  * @author Jason Barr
  * @version 0.1
  * @license MIT
  */
 
-/**
- * Instantiates EasyHTTP library
- * @constructor
- * @property {XMLHttpRequest} http Request object this class wraps around
- */
-let EasyHTTP = function() {
-    this.http = new XMLHttpRequest();
-}
+(function(window) {
 
-/**
- * Makes HTTP GET request
- * 
- * @param {string} url
- * @param {getResponseCallback} callback Callback function to handle data response
- */
-EasyHTTP.prototype.get = function(url, callback) {
-    let self = this;
+    /**
+     * Create private XMLHttpRequest object
+     * 
+     * @private
+     * @since 0.1
+     */
+    xhr = new XMLHttpRequest();
 
-    this.http.open('GET', url, true);
-
-    this.http.onload = function() {
-        if (self.http.status === 200) {
-            callback(self.http.responseText);
-        } else {
-            callback(null, `Error: ${self.http.status}`)
+    const EasyHTTPAjax = {
+        
+        /**
+         * Makes HTTP GET request
+         * 
+         * @param {string} url
+         * @param {getResponseCallback} callback Callback function to handle data response
+         * @since 0.1
+         */
+        get: function(url, callback) {
+        
+            xhr.open('GET', url, true);
+        
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    callback(xhr.responseText);
+                } else {
+                    callback(null, `Error: ${xhr.status}`)
+                }
+            }
+        
+            xhr.send();
+        },
+        /**
+         * Response callback requirements for EasyHTTP.get()
+         * 
+         * @callback getResponseCallback
+         * @param {string} responseText HTTP Request response text
+         * @param {string} [errorMessage] Error message HTTP status
+         */
+        
+        /**
+         * Makes HTTP POST request
+         * 
+         * @param {string} url
+         * @param {Object} data
+         * @param {postRequestCallback} callback
+         * @since 0.1
+         */
+        post: function(url, data, callback) {
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+        
+            xhr.onload = function() {
+                callback(xhr.responseText);
+            }
+        
+            xhr.send(JSON.stringify(data));
+        },
+        /**
+         * POST request callback requirements for EasyHTTP.post()
+         * @callback postRequestCallback
+         * @param {string} responseText HTTP response text
+         */
+        
+        /**
+         * Makes HTTP POST request
+         * 
+         * @param {string} url
+         * @param {Object} data
+         * @param {postRequestCallback} callback
+         * @since 0.1
+         */
+        put: function(url, data, callback) {
+            xhr.open('PUT', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+        
+            xhr.onload = function() {
+                callback(xhr.responseText);
+            }
+        
+            xhr.send(JSON.stringify(data));
+        },
+        /**
+         * PUT request callback requirements for EasyHTTP.put()
+         * @callback putRequestCallback
+         * @param {string} responseText HTTP response text
+         */
+        
+        /**
+         * Makes HTTP DELETE request
+         * 
+         * @param {string} url
+         * @param {deleteResponseCallback} callback Callback function to handle data response
+         * @since 0.1
+         */
+        delete: function(url, callback) {
+        
+            xhr.open('DELETE', url, true);
+        
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    callback(xhr.responseText);
+                } else {
+                    callback(null, `Error: ${xhr.status}`)
+                }
+            }
+        
+            xhr.send();
         }
     }
 
-    this.http.send();
-}
-/**
- * Response callback requirements for EasyHTTP.get()
- * 
- * @callback getResponseCallback
- * @param {string} responseText HTTP Request response text
- * @param {string} [errorMessage] Error message HTTP status
- */
-
-/**
- * Makes HTTP POST request
- * 
- * @param {string} url
- * @param {Object} data
- * @param {postRequestCallback} callback
- */
-EasyHTTP.prototype.post = function(url, data, callback) {
-    let self = this;
-    this.http.open('POST', url, true);
-    this.http.setRequestHeader('Content-Type', 'application/json');
-
-    this.http.onload = function() {
-        callback(self.http.responseText);
-    }
-
-    this.http.send(JSON.stringify(data));
-}
-/**
- * POST request callback requirements for EasyHTTP.post()
- * @callback postRequestCallback
- * @param {string} responseText HTTP response text
- */
-
-/**
- * Makes HTTP POST request
- * 
- * @param {string} url
- * @param {Object} data
- * @param {postRequestCallback} callback
- */
-EasyHTTP.prototype.put = function(url, data, callback) {
-    let self = this;
-    this.http.open('PUT', url, true);
-    this.http.setRequestHeader('Content-Type', 'application/json');
-
-    this.http.onload = function() {
-        callback(self.http.responseText);
-    }
-
-    this.http.send(JSON.stringify(data));
-}
-/**
- * PUT request callback requirements for EasyHTTP.put()
- * @callback putRequestCallback
- * @param {string} responseText HTTP response text
- */
-
-/**
- * Makes HTTP DELETE request
- * 
- * @param {string} url
- * @param {deleteResponseCallback} callback Callback function to handle data response
- */
-EasyHTTP.prototype.delete = function(url, callback) {
-    let self = this;
-
-    this.http.open('DELETE', url, true);
-
-    this.http.onload = function() {
-        if (self.http.status === 200) {
-            callback(self.http.responseText);
-        } else {
-            callback(null, `Error: ${self.http.status}`)
-        }
-    }
-
-    this.http.send();
-}
+    window.EasyHTTP = EasyHTTPAjax;
+})(window);
